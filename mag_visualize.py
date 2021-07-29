986227e9ae8c9f93309f3c07e9d815e7c1955b1a
yunsj111 <yunsj111@gmail.com>
2021-07-29 19:57:53 +0900

    `vmin` and `vmax` arguments are added to  function 'show_MagProperties'

--- mag_visualize.py
+++ mag_visualize.py
@@ -14,16 +14,19 @@ def show_mask(magnet, figsize=(5,5), fontsize=100):
         plt.title('nz : {}/{}'.format(i,total_nz), fontdict={'size': fontsize})
     plt.show()
     
-def show_MagProperties(mag_property, figsize=(5,5), fontsize=100):
+def show_MagProperties(mag_property, vmin=None, vmax=None,figsize=(5,5), fontsize=100):
     total_nz = mag_property.shape[0]
     d1 = total_nz // 5
     d2 = total_nz % 5
     if d2!=0:
         d1 += 1
-    vmax = mag_property.max()
+    if vmax==None:
+        vmax = mag_property.max()
+    if vmin==None:
+        vmin = mag_property.min()
     plt.figure(figsize=(figsize[0]*5, figsize[1]*d1),)
     for i in range(total_nz):
         plt.subplot(d1,5,i+1)
-        sns.heatmap(mag_property[i].tolist(), cmap='RdBu', vmin=-vmax, vmax=vmax, cbar=True, yticklabels=False, xticklabels=False).invert_yaxis()
+        sns.heatmap(mag_property[i].tolist(), cmap='RdBu', vmin=vmin, vmax=vmax, cbar=True, yticklabels=False, xticklabels=False).invert_yaxis()
         plt.title('nz : {}/{}'.format(i,total_nz), fontdict={'size': fontsize})
     plt.show()
