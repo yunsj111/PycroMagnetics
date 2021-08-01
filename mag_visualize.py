aaf76647f6261d860083dcb98cb68fc61baad09e
yunsj111 <yunsj111@gmail.com>
2021-08-01 09:52:55 +0900

    add arguments `square` and `shirink` to the heatmap functions

--- mag_visualize.py
+++ mag_visualize.py
@@ -10,11 +10,11 @@ def show_mask(magnet, figsize=(5,5), fontsize=100):
     plt.figure(figsize=(figsize[0]*5, figsize[1]*d1),)
     for i in range(total_nz):
         plt.subplot(d1,5,i+1)
-        sns.heatmap(magnet.mask[i].tolist(), cmap='gray', vmin=-1,vmax=1, cbar=False, yticklabels=False, xticklabels=False).invert_yaxis()
-        plt.title('nz : {}/{}'.format(i,total_nz), fontdict={'size': fontsize})
+        sns.heatmap(magnet.mask[i].tolist(), cmap='gray', vmin=-1,vmax=1, cbar=False, square=True, yticklabels=False, xticklabels=False).invert_yaxis()
+        plt.title('nz : {}/{}'.format(i+1,total_nz), fontdict={'size': fontsize})
     plt.show()
     
-def show_MagProperties(mag_property, vmin=None, vmax=None,figsize=(5,5), fontsize=100):
+def show_MagProperties(mag_property, vmin=None, vmax=None,figsize=(5,5), fontsize=100, shrink=0.8):
     total_nz = mag_property.shape[0]
     d1 = total_nz // 5
     d2 = total_nz % 5
@@ -27,6 +27,6 @@ def show_MagProperties(mag_property, vmin=None, vmax=None,figsize=(5,5), fontsiz
     plt.figure(figsize=(figsize[0]*5, figsize[1]*d1),)
     for i in range(total_nz):
         plt.subplot(d1,5,i+1)
-        sns.heatmap(mag_property[i].tolist(), cmap='RdBu', vmin=vmin, vmax=vmax, cbar=True, yticklabels=False, xticklabels=False).invert_yaxis()
+        sns.heatmap(mag_property[i].tolist(), cmap='RdBu', vmin=vmin, vmax=vmax, cbar=True, square=True, yticklabels=False, xticklabels=False, cbar_kws={"shrink": shrink}).invert_yaxis()
         plt.title('nz : {}/{}'.format(i,total_nz), fontdict={'size': fontsize})
     plt.show()
